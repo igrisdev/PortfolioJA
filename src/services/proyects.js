26928eb0590e84c3af2d48e8b8843d1c0cbccf41
TJIGRIS27m <johanalvarezzz237@gmail.com>
2023-11-21 16:09:46 -0500

    terminado el footer, redes sociales solo faltalas de verdad, head ya, falta el boton y los proyectos

---
+++ src/services/proyects.js
@@ -0,0 +1,33 @@
+export const projects = [
+  {
+    title: "Aplicacion del clima",
+    urlRepository: 'https://github.com/TJIGRIS/WEATHER-APP-MASTER-TJIGRIS.git',
+    urlWeb: 'https://gleeful-salmiakki-5ff1cd.netlify.app/',
+    sizeCard: 'small',
+    tecnologies: ''
+  },
+  {
+    title: "Generador de notas",
+    urlRepository: 'https://github.com/TJIGRIS/RANDOM-QUOTE-GENERATOR-TJIGRIS.git',
+    urlWeb: 'https://random-quote-generator-tjigris.netlify.app/quote',
+    sizeCard: 'big',
+  },
+  {
+    title: "Aplicacion lista de hacer",
+    urlRepository: 'https://github.com/TJIGRIS/TODO-APP-TJIGRIS.git',
+    urlWeb: 'https://tjigris.github.io/TODO-APP-TJIGRIS/',
+    sizeCard: 'big',
+  },
+  {
+    title: "",
+    urlRepository: '',
+    urlWeb: '',
+    sizeCard: 'big',
+  },
+  {
+    title: "Buscar apartamento",
+    urlRepository: 'https://github.com/TJIGRIS/WINDBNB-TJIGRIS.git',
+    urlWeb: 'https://tjigris.github.io/WINDBNB-TJIGRIS/',
+    sizeCard: 'small',
+  },
+]
