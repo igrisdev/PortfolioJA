763e0ebb210593e55c2e410edd13c482450b10d0
TJIGRIS27m <johanalvarezzz237@gmail.com>
2023-11-24 20:21:15 -0500

    Agregado experiencia, estudios, falta mas info y el boton de cambiar el color a la pagina

--- src/components/ButtonChangeColor.jsx
+++ src/components/ButtonChangeColor.jsx
@@ -1,35 +1,11 @@
-import { useEffect, useState } from "react";
-import img from "../../public/favicon.svg";
+import sun from "@assets/iconsChangeColor/sun.svg"
+import moon from "@assets/iconsChangeColor/moon.svg"
 
-
-export function Nav() {
-  const [changeColor, setChangeColor] = useState('white')
-
-  useEffect(() => {
-    const visible = () => {
-    }
-  }, [])
-
-
+export default function ButtonChangeColor() {
   return (
-    <div className="fixed top-0 left-0 right-0 z-20 bg-[#101964]/80  backdrop-blur-lg">
-      <nav className="max-w-3xl mx-auto flex items-center justify-between">
-        <header
-          class="flex gap-3 items-center py-2"
-        >
-          <picture
-            class="w-14 h-14 rounded-full shadow-red-600 overflow-hidden border-2 border-white"
-          >
-            <img src={img.src} alt="image" />
-          </picture>
-
-          <h1 class="text-base text-orange-400 font-bold">Johan Alvarez</h1>
-        </header>
-
-        <div>
-          boton
-        </div>
-      </nav>
-    </div>
+    <button>
+      <img src={sun.src} alt={sun.src} />
+      {/* <img src={moon.src} alt={moon.src} /> */}
+    </button>
   )
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'P-light-title-primary': 'rgb(251 146 60)',
+				'P-light-subtitle-primary': '#fff',
+				'P-light-text-primary': 'rgb(156 163 175)',
+				'P-light-text-secondary': '#000',
+				'P-light-backgound-primary': '#101964',
+				'P-light-backgound-secondary': 'rgb(251 146 60)',
+				'P-light-backgound-tertiary': '#fff',
+			}
+		},
 	},
 	plugins: [
 		require('tailwindcss-animated')
