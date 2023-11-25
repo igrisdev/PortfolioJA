334e9b47df2718a4f0e8410d948726dad5d67bb6
TJIGRIS27m <johanalvarezzz237@gmail.com>
2023-11-24 22:34:23 -0500

    arreglados los iconos y todo lo que tiene que ver con el boton de cambiar color y los colores de la pagina terminados, falta informacion real

--- src/components/ButtonChangeColor.jsx
+++ src/components/ButtonChangeColor.jsx
@@ -1,11 +1,45 @@
 import sun from "@assets/iconsChangeColor/sun.svg"
 import moon from "@assets/iconsChangeColor/moon.svg"
+import { useEffect, useState } from "react"
 
 export default function ButtonChangeColor() {
+  const [changeColor, setChangeColor] = useState(false)
+
+
+  const handleChangeColor = () => {
+    setChangeColor(prevChangeColor => !prevChangeColor)
+
+    if (!changeColor) {
+      localStorage.setItem('color', 'dark')
+      return
+    }
+
+    localStorage.setItem('color', 'white')
+  }
+
+  useEffect(() => {
+    const modeColor = localStorage.getItem('color')
+    const body = document.querySelector('body')
+
+    if (modeColor === 'dark') {
+      setChangeColor(true)
+      body.classList.add('dark')
+      console.log(modeColor);
+    } else if (modeColor === 'white') {
+      setChangeColor(false)
+      body.classList.remove('dark')
+      console.log(modeColor);
+    }
+  }, [changeColor])
+
+
+
   return (
-    <button>
-      <img src={sun.src} alt={sun.src} />
-      {/* <img src={moon.src} alt={moon.src} /> */}
+    <button onClick={handleChangeColor}>
+      {changeColor ?
+        <img src={moon.src} alt={moon.src} /> :
+        <img src={sun.src} alt={sun.src} />
+      }
     </button>
   )
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: ['class'],
 	theme: {
 		extend: {
 			colors: {
@@ -11,6 +12,14 @@ export default {
 				'P-light-backgound-primary': '#101964',
 				'P-light-backgound-secondary': 'rgb(251 146 60)',
 				'P-light-backgound-tertiary': '#fff',
+
+				'P-dark-title-primary': '#fff',
+				'P-dark-subtitle-primary': '#ccccc9',
+				'P-dark-text-primary': 'rgb(156 163 175)',
+				'P-dark-text-secondary': '#9facb8',
+				'P-dark-backgound-primary': '#1d2938',
+				'P-dark-backgound-secondary': '#2a3545',
+				'P-dark-backgound-tertiary': '#fff',
 			}
 		},
 	},
