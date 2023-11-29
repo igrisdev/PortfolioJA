62f5715423b8ea72228187e23571ad7fd68cf32f
TJIGRIS27m <johanalvarezzz237@gmail.com>
2023-11-28 23:30:47 -0500

    se elimino los iconos de las skills para subirlas a claudinary y usar el link directo

--- src/services/projects.js
+++ src/services/projects.js
@@ -4,7 +4,10 @@ export const projects = [
     urlRepository: 'https://github.com/TJIGRIS/WEATHER-APP-MASTER-TJIGRIS.git',
     urlWeb: 'https://gleeful-salmiakki-5ff1cd.netlify.app/',
     sizeCard: true,
-    skills: ['react', 'tailwind'],
+    skills: [
+      { url: 'https://res.cloudinary.com/dbwupri3k/image/upload/v1701231522/PortfolioJA/IconsSkills/ufpy5r6l0lsu5izjj1xu.svg', skill: 'react' },
+      { url: 'https://res.cloudinary.com/dbwupri3k/image/upload/v1701231522/PortfolioJA/IconsSkills/lzeyxmohh4plhwimeaod.svg', skill: 'tailwind' }
+    ],
     imageName: 'https://res.cloudinary.com/dbwupri3k/image/upload/v1701230671/PortfolioJA/ImageProjects/allvu6b9935bsghbw8pq.webp',
     description: 'App en la que aplique todos los conceptos que tengo hasta el momento, uso se hooks como useReducer y useContext.'
   },
@@ -13,7 +16,10 @@ export const projects = [
     urlRepository: 'https://github.com/TJIGRIS/RANDOM-QUOTE-GENERATOR-TJIGRIS.git',
     urlWeb: 'https://random-quote-generator-tjigris.netlify.app/',
     sizeCard: true,
-    skills: ['react', 'tailwind'],
+    skills: [
+      { url: 'https://res.cloudinary.com/dbwupri3k/image/upload/v1701231522/PortfolioJA/IconsSkills/ufpy5r6l0lsu5izjj1xu.svg', skill: 'react' },
+      { url: 'https://res.cloudinary.com/dbwupri3k/image/upload/v1701231522/PortfolioJA/IconsSkills/lzeyxmohh4plhwimeaod.svg', skill: 'tailwind' }
+    ],
     imageName: 'https://res.cloudinary.com/dbwupri3k/image/upload/v1701230672/PortfolioJA/ImageProjects/vjtj2kcdswsebtaknttx.webp',
     description: 'App en la que practique el hook useContext principalmente y llamada de una api con fetch.'
   },
@@ -22,7 +28,10 @@ export const projects = [
     urlRepository: 'https://github.com/TJIGRIS/TODO-APP-TJIGRIS.git',
     urlWeb: 'https://tjigris.github.io/TODO-APP-TJIGRIS/',
     sizeCard: false,
-    skills: ['react', 'tailwind'],
+    skills: [
+      { url: 'https://res.cloudinary.com/dbwupri3k/image/upload/v1701231522/PortfolioJA/IconsSkills/ufpy5r6l0lsu5izjj1xu.svg', skill: 'react' },
+      { url: 'https://res.cloudinary.com/dbwupri3k/image/upload/v1701231522/PortfolioJA/IconsSkills/lzeyxmohh4plhwimeaod.svg', skill: 'tailwind' }
+    ],
     imageName: 'todo',
     description: 'App en la que aplique los conceptos de rutas con la librería React Router.'
   },
@@ -31,7 +40,10 @@ export const projects = [
     urlRepository: 'https://github.com/TJIGRIS/WINDBNB-TJIGRIS.git',
     urlWeb: 'https://tjigris.github.io/WINDBNB-TJIGRIS/',
     sizeCard: false,
-    skills: ['react', 'tailwind'],
+    skills: [
+      { url: 'https://res.cloudinary.com/dbwupri3k/image/upload/v1701231522/PortfolioJA/IconsSkills/ufpy5r6l0lsu5izjj1xu.svg', skill: 'react' },
+      { url: 'https://res.cloudinary.com/dbwupri3k/image/upload/v1701231522/PortfolioJA/IconsSkills/lzeyxmohh4plhwimeaod.svg', skill: 'tailwind' }
+    ],
     imageName: 'hotels',
     description: 'App en la que aprendí sobre useContext.'
   },
