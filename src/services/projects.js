6a56aaf62e11d58354c09ea20af8d622e1d1b1d3
igrisdev <johanalvarezz237@gmail.com>
2024-08-27 13:45:36 -0500

    nuevo intento

--- src/services/projects.js
+++ src/services/projects.js
@@ -1,5 +1,54 @@
-
-export const projects = (await api()) || []
+export const projects = [
+  {
+    title: 'Horario UniMayor',
+    urlRepository: 'https://github.com/igrisdev/horario_unimayor',
+    urlWeb: 'https://horario-unimayor.vercel.app',
+    sizeCard: true,
+    skills: [
+      {
+        url: 'https://res.cloudinary.com/dbwupri3k/image/upload/v1718679763/PortfolioJA/IconsSkills/x0oa6esfpleclrhwkt1w.svg',
+        skill: 'nest js',
+      },
+      {
+        url: 'https://res.cloudinary.com/dbwupri3k/image/upload/v1701231522/PortfolioJA/IconsSkills/lzeyxmohh4plhwimeaod.svg',
+        skill: 'tailwind',
+      },
+      {
+        url: 'https://res.cloudinary.com/dbwupri3k/image/upload/v1718679848/PortfolioJA/IconsSkills/bfaxtmcl9iyuurrf2zbt.svg',
+        skill: 'prisma',
+      },
+      {
+        url: 'https://res.cloudinary.com/dbwupri3k/image/upload/v1718679677/PortfolioJA/IconsSkills/ycblmxtxdng8iqhobbih.svg',
+        skill: 'PostgreSQL',
+      },
+    ],
+    imageName:
+      'https://res.cloudinary.com/dbwupri3k/image/upload/v1718679486/PortfolioJA/ImageProjects/kyqyewxo5yhmhzkvhzfk.jpg',
+    description:
+      'App desarrollada con Next js, cuenta con una base de datos y se realizado para mejorar la experiencia al momento de asignar los horarios de clases a los docentes.',
+  },
+  {
+    title: 'Aplicación del clima',
+    urlRepository: 'https://github.com/TJIGRIS/WEATHER-APP-MASTER-TJIGRIS.git',
+    urlWeb: 'https://gleeful-salmiakki-5ff1cd.netlify.app/',
+    sizeCard: true,
+    skills: [
+      {
+        url: 'https://res.cloudinary.com/dbwupri3k/image/upload/v1701231522/PortfolioJA/IconsSkills/ufpy5r6l0lsu5izjj1xu.svg',
+        skill: 'react',
+      },
+      {
+        url: 'https://res.cloudinary.com/dbwupri3k/image/upload/v1701231522/PortfolioJA/IconsSkills/lzeyxmohh4plhwimeaod.svg',
+        skill: 'tailwind',
+      },
+    ],
+    imageName:
+      'https://res.cloudinary.com/dbwupri3k/image/upload/v1701230671/PortfolioJA/ImageProjects/allvu6b9935bsghbw8pq.webp',
+    description:
+      'App en la que aplique todos los conceptos que tengo hasta el momento, uso de hooks como useReducer y useContext.',
+  },
+]
+// export const projects = (await api()) || []
 
 async function api() {
   const document = await fetch(
