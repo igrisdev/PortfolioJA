62e091fd05a877b7ae3d98eb8f178e2d1509cde9
igrisdev <johanalvarezz237@gmail.com>
2024-09-04 09:22:59 -0500

    Proved

--- src/components/ContainerProjects.jsx
+++ src/components/ContainerProjects.jsx
@@ -4,33 +4,49 @@ import { ButtonCardProjects } from './ButtonCardProjects'
 import { useEffect, useState } from 'react'
 
 export const ContainerProjects = () => {
-  const [projects, setProjects] = useState([])
+  const [projects, setProjects] = useState(null)
 
   useEffect(() => {
     setProjects(API)
   }, [])
 
+  if (projects === null)
+    return (
+      <div className='grid place-content-center text-lg py-10 text-P-title-primary'>
+        <h3>Cargando proyectos...</h3>
+      </div>
+    )
+
+  if (projects.length === 0)
+    return (
+      <div className='grid place-content-center text-lg py-10 text-P-title-primary'>
+        <h3>No hay proyectos...</h3>
+      </div>
+    )
+
   return (
     <div className='flex flex-col gap-8 mt-4 mb-16'>
-      {projects?.map((item) => {
-        if (item.sizeCard) {
+      {projects.map((item) => {
+        if (item.sizeCard)
           return (
             <CardProjects
               key={item.urlRepository}
               {...item}
             />
           )
-        } else {
-          return (
-            <div
-              className='grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-8'
-              key={item.urlRepository}
-            >
-              <ButtonCardProjects {...item} />
-            </div>
-          )
-        }
       })}
+
+      <div className='grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-8'>
+        {projects.map((item) => {
+          if (!item.sizeCard)
+            return (
+              <ButtonCardProjects
+                key={item.urlRepository}
+                {...item}
+              />
+            )
+        })}
+      </div>
     </div>
   )
 }
--- src/services/projects.js
+++ src/services/projects.js
@@ -1,3 +1,53 @@
+export const projects = (await api()) || []
+
+async function api() {
+  const document = await fetch(
+    'https://docs.google.com/spreadsheets/d/1ChnRtdQcb8bQ_WiS43gHEKsagiLmpzMkkh7MSNhIaLg/pub?gid=537540525&single=true&output=tsv'
+  ).then((res) => res.text())
+
+  const rows = document
+    .split('\n')
+    .slice(1)
+    .map((row) => row.trim().split('\t'))
+
+  return rows.map(
+    ([
+      title,
+      urlRepository,
+      urlWeb,
+      sizeCard,
+      skills,
+      imageName,
+      description,
+    ]) => ({
+      title,
+      urlRepository,
+      urlWeb,
+      sizeCard: sizeCard === 'TRUE',
+      skills: parseSkills(skills),
+      imageName,
+      description,
+    })
+  )
+}
+
+function parseSkills(skillsString) {
+  const skillEntries = skillsString
+    .replace(/[\r\n]+/g, '')
+    .split('},')
+    .map((skill) => skill.trim() + '}')
+
+  return skillEntries.map((skill) => {
+    const urlMatch = skill.match(/url:\s*'([^']+)'/)
+    const skillMatch = skill.match(/skill:\s*'([^']+)'/)
+
+    return {
+      url: urlMatch ? urlMatch[1] : '',
+      skill: skillMatch ? skillMatch[1] : '',
+    }
+  })
+}
+
 /* export const projects = [
   {
     title: 'UniMayor',
@@ -48,52 +98,3 @@
       'App en la que aplique todos los conceptos que tengo hasta el momento, uso de hooks como useReducer y useContext.',
   },
 ] */
-export const projects = (await api()) || []
-
-async function api() {
-  const document = await fetch(
-    'https://docs.google.com/spreadsheets/d/1ChnRtdQcb8bQ_WiS43gHEKsagiLmpzMkkh7MSNhIaLg/pub?gid=537540525&single=true&output=tsv'
-  ).then((res) => res.text())
-
-  const rows = document
-    .split('\n')
-    .slice(1)
-    .map((row) => row.trim().split('\t'))
-
-  return rows.map(
-    ([
-      title,
-      urlRepository,
-      urlWeb,
-      sizeCard,
-      skills,
-      imageName,
-      description,
-    ]) => ({
-      title,
-      urlRepository,
-      urlWeb,
-      sizeCard: sizeCard === 'TRUE',
-      skills: parseSkills(skills),
-      imageName,
-      description,
-    })
-  )
-}
-
-function parseSkills(skillsString) {
-  const skillEntries = skillsString
-    .replace(/[\r\n]+/g, '')
-    .split('},')
-    .map((skill) => skill.trim() + '}')
-
-  return skillEntries.map((skill) => {
-    const urlMatch = skill.match(/url:\s*'([^']+)'/)
-    const skillMatch = skill.match(/skill:\s*'([^']+)'/)
-
-    return {
-      url: urlMatch ? urlMatch[1] : '',
-      skill: skillMatch ? skillMatch[1] : '',
-    }
-  })
-}
