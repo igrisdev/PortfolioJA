15b7a59ada6a264db88c0e09ad33fd9f3661ebfe
igrisdev <johanalvarezz237@gmail.com>
2024-09-04 08:38:58 -0500

    fixed class to className

--- src/components/ButtonCardProjects.jsx
+++ src/components/ButtonCardProjects.jsx
@@ -6,25 +6,27 @@ export const ButtonCardProjects = ({
   urlRepository,
 }) => {
   return (
-    <article class='shadow ring-[2px] hover:ring-4 ring-P-background-secondary flex flex-col justify-center px-6 pt-6 pb-3 rounded-lg transition-all duration-500 md:hover:-translate-y-2 relative group'>
+    <article className='shadow ring-[2px] hover:ring-4 ring-P-background-secondary flex flex-col justify-center px-6 pt-6 pb-3 rounded-lg transition-all duration-500 md:hover:-translate-y-2 relative group'>
       <a
         href={urlWeb}
         target='_blank'
       >
-        <h2 class='text-P-subtitle-primary text-xl font-semibold'>{title}</h2>
-        <p class='text-P-text-primary text-sm'>{description}</p>
+        <h2 className='text-P-subtitle-primary text-xl font-semibold'>
+          {title}
+        </h2>
+        <p className='text-P-text-primary text-sm'>{description}</p>
 
         {/* <ListIcons skills={skills} /> */}
       </a>
 
-      <div class='absolute top-4 right-4 text-xl opacity-0 text-P-title-primary group-hover:opacity-100 group-hover:animate-jump animate-duration-300 animate-ease-linear z-10'>
+      <div className='absolute top-4 right-4 text-xl opacity-0 text-P-title-primary group-hover:opacity-100 group-hover:animate-jump animate-duration-300 animate-ease-linear z-10'>
         {/* <RowIcon /> */}
       </div>
 
-      <div class='absolute bottom-4 right-4 text-xl opacity-0 group-hover:opacity-100 group-hover:animate-jump animate-duration-300 animate-ease-linear z-10'>
+      <div className='absolute bottom-4 right-4 text-xl opacity-0 group-hover:opacity-100 group-hover:animate-jump animate-duration-300 animate-ease-linear z-10'>
         <a
           href={urlRepository}
-          class='bg-P-background-tertiary text-P-text-secondary p-1 text-xs rounded-lg font-semibold z-10 hover:bg-P-background-secondary
+          className='bg-P-background-tertiary text-P-text-secondary p-1 text-xs rounded-lg font-semibold z-10 hover:bg-P-background-secondary
       transition-all duration-200 dark:hover:text-P-title-primary'
           target='_blank'
         >
--- src/components/CardProjects.jsx
+++ src/components/CardProjects.jsx
@@ -8,31 +8,31 @@ export const CardProjects = ({
 }) => {
   console.log(title, description)
   return (
-    <article class='grid grid-cols-1 md:grid-cols-2 gap-8 group'>
+    <article className='grid grid-cols-1 md:grid-cols-2 gap-8 group'>
       <a
         href={urlWeb}
         target='_blank'
-        class='w-full h-full max-h-[350px] md:h-56 bg-P-background-secondary rounded-lg overflow-hidden'
+        className='w-full h-full max-h-[350px] md:h-56 bg-P-background-secondary rounded-lg overflow-hidden'
       >
         <picture>
           <img
-            class='w-full h-full translate-x-8 translate-y-8 group-hover:translate-x-6 group-hover:translate-y-6 rounded-lg transition-all duration-500'
+            className='w-full h-full translate-x-8 translate-y-8 group-hover:translate-x-6 group-hover:translate-y-6 rounded-lg transition-all duration-500'
             src={imageName}
             alt={title}
           />
         </picture>
       </a>
 
-      <section class='flex flex-col gap-2 justify-center'>
-        <h2 class='text-P-subtitle-primary text-3xl font-semibold'>{title}</h2>
-        <p class='text-P-text-primary text-sm'>{description}</p>
+      <section className='flex flex-col gap-2 justify-center'>
+        <h2 className='text-P-subtitle-primary text-3xl font-semibold'>{title}</h2>
+        <p className='text-P-text-primary text-sm'>{description}</p>
 
         {/* <ListIcons skills={skills} /> */}
 
-        <div class='flex gap-3'>
+        <div className='flex gap-3'>
           <a
             href={urlRepository}
-            class='bg-P-background-tertiary text-P-text-secondary px-4 py-2 rounded-lg font-semibold'
+            className='bg-P-background-tertiary text-P-text-secondary px-4 py-2 rounded-lg font-semibold'
             target='_blank'
           >
             Code
@@ -40,7 +40,7 @@ export const CardProjects = ({
 
           <a
             href={urlWeb}
-            class='bg-P-background-tertiary text-P-text-secondary flex gap-2 hover:gap-5 transition-all duration-200 px-4 py-2 rounded-lg font-semibold group/link'
+            className='bg-P-background-tertiary text-P-text-secondary flex gap-2 hover:gap-5 transition-all duration-200 px-4 py-2 rounded-lg font-semibold group/link'
             target='_blank'
           >
             Live
