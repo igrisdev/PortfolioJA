03525e75059c3c3e102947215630bda79dcd9d84
igrisdev <johanalvarezz237@gmail.com>
2024-09-04 09:13:50 -0500

    Fixed

--- src/components/ContainerProjects.jsx
+++ src/components/ContainerProjects.jsx
@@ -1,31 +1,36 @@
-import { projects } from '@services/projects'
+import { projects as API } from '@services/projects'
 import { CardProjects } from './CardProjects'
 import { ButtonCardProjects } from './ButtonCardProjects'
+import { useEffect, useState } from 'react'
 
 export const ContainerProjects = () => {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    setProjects(API)
+  }, [])
+
   return (
     <div className='flex flex-col gap-8 mt-4 mb-16'>
-      {projects.map((item) => {
-        if (item.sizeCard)
+      {projects?.map((item) => {
+        if (item.sizeCard) {
           return (
             <CardProjects
               key={item.urlRepository}
               {...item}
             />
           )
+        } else {
+          return (
+            <div
+              className='grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-8'
+              key={item.urlRepository}
+            >
+              <ButtonCardProjects {...item} />
+            </div>
+          )
+        }
       })}
-
-      <div className='grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-8'>
-        {projects.map((item) => {
-          if (!item.sizeCard)
-            return (
-              <ButtonCardProjects
-                key={item.urlRepository}
-                {...item}
-              />
-            )
-        })}
-      </div>
     </div>
   )
 }
