3f06d25009b3216fd633f95dfae871c77294b59d
igrisdev <johanalvarezz237@gmail.com>
2024-11-06 21:14:30 -0600

    n

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,24 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: ['class'],
-	theme: {
-		extend: {
-			colors: {
-				'P-title-primary': 'var(--title-primary)',
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: ["class"],
+  theme: {
+    extend: {
+      colors: {
+        "P-title-primary": "var(--title-primary)",
 
-				'P-subtitle-primary': 'var(--subtitle-primary)',
+        "P-subtitle-primary": "var(--subtitle-primary)",
 
-				'P-text-primary': 'var(--text-primary)',
-				'P-text-secondary': 'var(--text-secondary)',
+        "P-text-primary": "var(--text-primary)",
+        "P-text-secondary": "var(--text-secondary)",
 
-				'P-background-primary': 'var(--background-primary)',
-				'P-background-secondary': 'var(--background-secondary)',
-				'P-background-tertiary': 'var(--background-tertiary)'
-			}
-		},
-	},
-	plugins: [
-		require('tailwindcss-animated')
-	],
-}
+        "P-background-primary": "var(--background-primary)",
+        "P-background-secondary": "var(--background-secondary)",
+        "P-background-tertiary": "var(--background-tertiary)",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animated")],
+};
+
